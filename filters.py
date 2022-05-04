f25503103aad8896c7674b423699d5d4be85c672
Rafael-Cirino <r223730@dac.unicamp.br>
2022-05-04 12:49:43 -0300

    Filtros add

--- filters.py
+++ filters.py
@@ -15,7 +15,52 @@ class filters:
 
         return image_ocr
     
+    def get_grayscale(self, image):
+        npimagem = np.asarray(image).astype(np.uint8)
+        return cv2.cvtColor(npimagem, cv2.COLOR_BGR2GRAY)
+    
     def trunc_bin(self, image_ocr):
-        ret, thresh = cv2.threshold(image_ocr, 127, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU) 
 
-        return thresh
+        return cv2.threshold(image_ocr, 127, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
+        #return cv2.threshold(image_ocr, 127, 255, cv2.THRESH_TRUNC)[1]
+    
+    # noise removal
+    def remove_noise(self, image):
+        return cv2.medianBlur(image,5)
+
+    #dilation
+    def dilate(self, image):
+        kernel = np.ones((2,2),np.uint8)
+        return cv2.dilate(image, kernel, iterations = 1)
+        
+    #erosion
+    def erode(self, image):
+        kernel = np.ones((2,2),np.uint8)
+        return cv2.erode(image, kernel, iterations = 1)
+
+    #opening - erosion followed by dilation
+    def opening(self, image):
+        kernel = np.ones((2,2),np.uint8)
+        return cv2.morphologyEx(image, cv2.MORPH_OPEN, kernel)
+
+    #canny edge detection
+    def canny(self, image):
+        return cv2.Canny(image, 100, 200)
+
+    #skew correction
+    def deskew(self, image):
+        coords = np.column_stack(np.where(image > 0))
+        angle = cv2.minAreaRect(coords)[-1]
+        if angle < -45:
+            angle = -(90 + angle)
+        else:
+            angle = -angle
+        (h, w) = image.shape[:2]
+        center = (w // 2, h // 2)
+        M = cv2.getRotationMatrix2D(center, angle, 1.0)
+        rotated = cv2.warpAffine(image, M, (w, h), flags=cv2.INTER_CUBIC, borderMode=cv2.BORDER_REPLICATE)
+        return rotated
+
+    #template matching
+    def match_template(self, image, template):
+        return cv2.matchTemplate(image, template, cv2.TM_CCOEFF_NORMED)
--- main.py
+++ main.py
@@ -3,26 +3,36 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+PATH_TESSERACT = os.getenv("PATH_TESSERACT")
+PAST_TEST = os.getenv("PAST_TEST")
+
 import pytesseract as ocr
 from PIL import Image
 
+ocr.pytesseract.tesseract_cmd = PATH_TESSERACT  # Defini o caminho para Tesseract
 
 from filters import filters
+
 fi = filters()
 
-PATH_TESSERACT = os.getenv('PATH_TESSERACT')
-PAST_TEST = os.getenv('PAST_TEST')
-ocr.pytesseract.tesseract_cmd = PATH_TESSERACT
-
 # Colocando os canais em ordem RGB
-image_ocr = Image.open(PAST_TEST + 'nota-fiscal_dedo.jpg').convert('RGB')
+image_ocr = Image.open(PAST_TEST + "n_5.jpg").convert("RGB")
 
 image_ocr = fi.dim_ruido(image_ocr)
+
+# image_ocr = fi.get_grayscale(image_ocr)
+# image_ocr = fi.remove_noise(image_ocr)
 image_ocr = fi.trunc_bin(image_ocr)
 
-#Reconvertendo a imagem em um tipo Image.PIL
-image_ocr = Image.fromarray(image_ocr) 
+# image_ocr = fi.canny(image_ocr)
+# image_ocr = fi.dilate(image_ocr)
+# image_ocr = fi.erode(image_ocr)
+# image_ocr = fi.opening(image_ocr)
+
+# Reconvertendo a imagem em um tipo Image.PIL
+image_ocr = Image.fromarray(image_ocr)
+
 image_ocr.show()
-phrase = ocr.image_to_string(image_ocr, lang='por')
+phrase = ocr.image_to_string(image_ocr, lang="por")
 print(phrase)
 print("fim")
